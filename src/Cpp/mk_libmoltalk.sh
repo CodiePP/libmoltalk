7bd250de938db069cc619f931afd04810073f79b
Alexander V. Diemand <codieplusplus@apax.net>
2016-05-11 23:33:53 +0200

    new CI

--- src/Cpp/mk_libmoltalk.sh
+++ src/Cpp/mk_libmoltalk.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-for HPP in `../../../gitalk/utils/find_hpp.sh ../../Code/Cpp/libmoltalk.md`; do
-  ../../../gitalk/utils/make_hpp.sh ${HPP}
-  ../../../gitalk/utils/make_cpp.sh ${HPP}
+for HPP in `bash ../../../gitalk/utils/find_hpp.sh ../../Code/Cpp/libmoltalk.md`; do
+  bash ../../../gitalk/utils/make_hpp.sh ${HPP}
+  bash ../../../gitalk/utils/make_cpp.sh ${HPP}
 done
 
 
 cd tests
 
-../../../../gitalk/utils/make_test.sh  ../../../Code/Cpp/tests/utlibmoltalk.md 
+bash ../../../../gitalk/utils/make_test.sh  ../../../Code/Cpp/tests/utlibmoltalk.md 
 
 cd ..
